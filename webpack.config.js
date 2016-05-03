63c4c3d01645a8de5d555ff10da6fcdf66afd53c
Robby Emmert <robby.emmert@cqlcorp.com>
2016-05-03 12:20:54 -0400

    added dynamic html templating for webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
 	entry: './src/index.js',
 	output: {
@@ -9,7 +11,17 @@ module.exports = {
 			{
 				test: /\.s?css$/,
 				loaders: ['style', 'css', 'sass']
+			},
+			{
+				test: /\.hbs$/,
+				loaders: ['handlebars']
 			}
 		]
-	}
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Webpack Build',
+			template: './src/index.hbs'
+		})
+	]
 }
