d164b0d57b7b4cf60c20566f954a89a10e1f5343
Robby Emmert <robby.emmert@cqlcorp.com>
2016-05-04 12:15:49 -0400

    more documentation

--- webpack.config.js
+++ webpack.config.js
@@ -8,15 +8,15 @@ module.exports = {
 	},
 	module: {
 		loaders: [
-			{
+			{	// CSS/Sass loader config
 				test: /\.s?css$/,
 				loaders: ['style', 'css', 'sass']
 			},
-			{
+			{	// Handlebars loader config (optional)
 				test: /\.hbs$/,
 				loaders: ['handlebars']
 			},
-			{
+			{	// ES6 loader config
 				test: /\.jsx?$/,
 				exclude: /(node_modules|bower_components)/,
 				loaders: ['babel']
