a7629a6d036363a19fef276c81e7bd33ac8be02c
Robby Emmert <robby.emmert@cqlcorp.com>
2016-06-24 15:02:36 -0400

    webpack update

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,46 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	entry: './src/app.jsx',
-	output: {
-		path: __dirname + '/dist',
-		filename: 'bundle.js'
-	},
-	devtool: 'source-map',
-	module: {
-		loaders: [
-			{	// CSS/Sass loader config
-				test: /\.s?css$/,
-				loaders: ['style', 'css', 'sass']
-			},
-			{	// ES6 loader config
-				test: /\.jsx?$/,
-				exclude: /(node_modules|bower_components)/,
-				loaders: ['babel']
-			},
-			{
+    entry: './src/app.jsx',
+    output: {
+        path: __dirname + '/public',
+        filename: 'js/app.js'
+    },
+    devtool: 'source-map',
+    module: {
+        loaders: [
+            {    // CSS/Sass loader config
+                test: /\.s?css$/,
+                loaders: ['style', 'css', 'sass']
+            },
+            {    // ES6 loader config
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loaders: ['babel']
+            },
+            {
                 test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
                 loaders: ['file?name=fonts/[name].[ext]']
             }
-		]
-	},
-	plugins: [
-		new HtmlWebpackPlugin({		// Plugin to inject source references into index.html (note: doesn't need handlebars)
-			title: 'Webpack Build',
-			template: './src/index.html'
-		})
-	]
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({        // Plugin to inject source references into index.html (note: doesn't need handlebars)
+            title: 'Webpack Build',
+            template: './src/index.html'
+        })
+    ],
+    devServer: {
+        historyApiFallback: true,
+        contentBase: './public',
+        proxy: [
+            {
+                path: new RegExp('/api/(.*)'),
+                changeOrigin: true,
+                prependPath: false,
+                xfwd: true,
+                target: 'https://yourApiDomain.net/'
+            }
+        ]
+    }
 }
